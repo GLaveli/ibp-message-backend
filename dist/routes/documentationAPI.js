0173af1a3f4f7d2c4ce59d61f85299d6336cf6b1
GLaveli <g.laveli.p@gmail.com>
2021-05-02 22:28:08 -0300

    update

--- dist/routes/documentationAPI.js
+++ dist/routes/documentationAPI.js
@@ -5,5 +5,8 @@ var express_1 = require("express");
 var documentationAPI = express_1.Router();
 exports.documentationAPI = documentationAPI;
 documentationAPI.get('/', function (req, res) {
-    return res.status(200).json({ messa: 'Api esta funcionando cheers!!!' });
+    return res.status(200).json({
+        message: 'Api de integração de menssagens esta funcionando cheers!!!',
+        doc: 'Em breve documentação!',
+    });
 });
