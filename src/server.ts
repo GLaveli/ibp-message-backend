4085921f8707041f0351d50256d11334af5c9b05
GLaveli <g.laveli.p@gmail.com>
2021-05-03 17:46:44 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { messageRoutes } from './routes/message.routes';
 
 const PORT = process.env.PORT || 3333;
 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,5 +14,5 @@ app.use('/message', messageRoutes);
 app.use('/', documentationAPI);
 
 app.listen(PORT, () => {
- console.info(`Server started at port: ${PORT}`);
+ console.info(`Server started at port: ${PORT}!`);
 });
